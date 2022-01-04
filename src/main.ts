08ebde2ed824a65536fd126761e5cd346fd0d45f
Ozcan Ovunc <ozcanovunc@outlook.com>
2022-01-04 18:19:26 +0300

    feat(): Add centralized error mechanism

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app';
+import { HttpExceptionFilter } from './lib/exceptions/HttpException';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter)
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
